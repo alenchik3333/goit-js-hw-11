d0b554b8567ddc3e9575ae79b71d816d7548e968
alenchik3333 <113861539+alenchik3333@users.noreply.github.com>
2024-07-22 19:49:38 +0000

    Deploying to gh-pages from @ alenchik3333/goit-js-hw-11@b51f122cd98e3366e43002129e1d0424fe0e8279 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,11 @@
-import{S as y,i as n}from"./assets/vendor-0fc460d7.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function p(s){return fetch(`https://pixabay.com/api?key=45032349-419aa61286db3245b797ba166&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const c=document.querySelector(".gallery");let l;function m(s){return s.map(({webformatURL:i,largeImageURL:t,tags:a,likes:e,views:r,comments:o,downloads:f})=>`
+import{S as p,i as c}from"./assets/vendor-0fc460d7.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();function y(s){return fetch(`https://pixabay.com/api?key=45032349-419aa61286db3245b797ba166&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()})}const u=document.querySelector(".gallery");let n;function m(s){return s.map(({webformatURL:o,largeImageURL:i,tags:r,likes:e,views:t,comments:a,downloads:f})=>`
   <li class="gallery-item">
   <div class="overley-image">
-    <a class="gallery-link" href="${t}">
+    <a class="gallery-link" href="${i}">
       <img
         class="gallery-image"
-        src="${i}" 
-        alt="${a}"
+        src="${o}" 
+        alt="${r}"
       />
     </a>
     </div>
@@ -16,11 +16,11 @@ import{S as y,i as n}from"./assets/vendor-0fc460d7.js";(function(){const i=docum
     </div>
     <div class="info-detail">
         <h2 class="title">Views</h2>
-        <p class="text">${r}</p>
+        <p class="text">${t}</p>
     </div>
     <div class="info-detail">
         <h2 class="title">Comments</h2>
-        <p class="text">${o}</p>
+        <p class="text">${a}</p>
     </div>
     <div class="info-detail">
         <h2 class="title">Downloads</h2>
@@ -28,5 +28,5 @@ import{S as y,i as n}from"./assets/vendor-0fc460d7.js";(function(){const i=docum
     </div>
   </div>
   </li>
-  `).join("")}function h(s){l&&l.refresh(),c.innerHTML="",c.insertAdjacentHTML("afterbegin",m(s)),l=new y(".overley-image a",{captionsData:"alt",captionDelay:250})}const d=document.querySelector(".loading"),u=document.querySelector(".search-form");u.addEventListener("submit",s=>{s.preventDefault(),d.style.display="block",p(u.elements.query.value.toLowerCase()).then(t=>{t.totalHits===0?n.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(console.log(t.total),h(t.hits))}).catch(t=>{console.log(t),n.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"})}).finally(()=>{d.style.display="none"})});
+  `).join("")}function g(s){n&&n.refresh(),u.innerHTML="",u.insertAdjacentHTML("afterbegin",m(s)),n=new p(".overley-image a",{captionsData:"alt",captionDelay:250})}const d=document.querySelector(".loading"),l=document.querySelector(".search-form");l.addEventListener("submit",s=>{s.preventDefault(),l.elements.query.value.trim().toLowerCase()!=""&&(d.style.display="block",y(l.elements.query.value.toLowerCase()).then(r=>{r.totalHits===0?c.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(console.log(r.total),g(r.hits),l.elements.query.value="")}).catch(r=>{console.log(r),c.error({message:`Server error: ${r.message}`,position:"topRight"})}).finally(()=>{d.style.display="none"}))});
 //# sourceMappingURL=commonHelpers.js.map
